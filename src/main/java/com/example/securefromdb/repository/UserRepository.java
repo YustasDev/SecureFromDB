89d85020890f1b12d93178cff4811e60b98a7244
vitaly.gusev <vitaly.gusev@progforce.com>
2023-05-05 16:23:13 +0300

    todo 1

---
+++ src/main/java/com/example/securefromdb/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.securefromdb.repository;
+
+import com.example.securefromdb.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findUserByUsername(String username);
+}
